76d88a466a43b5b5736df520700c03c94a1d53d2
NikitaMlk <125889319+NikitaMlk@users.noreply.github.com>
2023-04-20 19:44:19 +0300

    Add files via upload

---
+++ lab_5/scr/webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+  entry: '/index.js',
+  output: {
+    filename: 'main.js',
+    path: __dirname + '/dist',
+  },
+  plugins: [
+    
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './pages/index.html',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './pages/news.html',
+      filename: 'news.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './pages/photo.html',
+      filename: 'photo.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './pages/rozklad.html',
+      filename: 'rozklad.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'assets/images', to: 'assets/images',
+        },
+      ]
+    })
+  ],
+  devServer: {
+    static: {
+      directory: __dirname + '/dist',
+    },
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+};
